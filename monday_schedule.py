68fd9a5dbc86bf2f703723df9dcb61a884eb319f
pujithaj <70451557+pujithaj@users.noreply.github.com>
2020-10-12 15:38:51 -0700

    Create monday_schedule.py

---
+++ monday_schedule.py
@@ -0,0 +1,8 @@
+print("monday : 7:45-8:50 zero period")
+print("monday : 8:50-9:10 break/")
+print("monday : 9:10-10:15 period one")
+print("monday : 10-15-10:35 break two")
+print("monday : 10:35-11:40 period two")
+print("monday : 11:40-12:10 lunch break")
+print("monday : 12:10-1:15  period three")
+print("1:15-3:00 independent work time")
